fc931f1cbec7dd0dd4e7c9127c0187dd56d90689
Jon Phipps <jphipps@madcreek.com>
2017-02-05 09:55:18 -0500

    Additional updates based on Laravel Shift suggestions
    Note: package.json still needs work

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -5,9 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
 
-/**
- * Class BroadcastServiceProvider.
- */
 class BroadcastServiceProvider extends ServiceProvider
 {
     /**
@@ -19,11 +16,6 @@ class BroadcastServiceProvider extends ServiceProvider
     {
         Broadcast::routes();
 
-        /*
-         * Authenticate the user's personal channel...
-         */
-        Broadcast::channel('App.User.*', function ($user, $userId) {
-            return (int) $user->id === (int) $userId;
-        });
+        require base_path('routes/channels.php');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,15 @@ use App\Models\Access\User\User;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 /**
  * Class TestCase.
  */
-abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+abstract class TestCase extends BaseTestCase
 {
     use DatabaseTransactions;
+	use CreatesApplication;
 
     /**
      * The base URL to use while testing the application.
@@ -52,19 +54,6 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     protected $userRole;
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
 
     /**
      * Set up tests.
